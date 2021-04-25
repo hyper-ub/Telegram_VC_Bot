b2fd6c1b0501b7662019a43214b3609d6a30a63b
Hyper <70685160+hyper-ub@users.noreply.github.com>
2021-04-25 09:54:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ if not HEROKU:
     API_ID = 14371
     API_HASH = "e46b6c854d2bf58a0"
     SUDO_CHAT_ID = -1001485876964
-    SUDOERS = [1243703097, 13216546]
+    SUDOERS = []
 
 # don't make changes below this line
 ARQ_API = "https://thearq.tech"
